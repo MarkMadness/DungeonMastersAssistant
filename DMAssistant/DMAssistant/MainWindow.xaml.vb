fc4799553028d65a01fb93d2d00b16e37433689b
Mark Roenfeldt <1187productions@gmail.com>
2021-06-07 07:28:05 -0600

    added notes for my use

--- DMAssistant/DMAssistant/MainWindow.xaml.vb
+++ DMAssistant/DMAssistant/MainWindow.xaml.vb
@@ -1,13 +1,13 @@
 ﻿Class MainWindow
     Private Sub WhereIsXur_Click(sender As Object, e As RoutedEventArgs)
         'Fill in all cities that players can currently go to. Later maybe select country first then from that select city
-        Dim cities As New List(Of String)({"0 Index", "The Sunken City", "Viluun", "O'lan", "Vex", "Erengard", "North Rigton", "Ashvar", "Bromwich", "Bracklewhyte", "Azurehill", "Wharfton", "Kingdom of Storms Outskirts", "Kingdom of Storms",
-                                          "Duel'Tharmen", "Thrandul",
-                                          "Loria, The Angel's Harbor", "[X'thul city]", "Tridenthia",
-                                          "Dul'tharion's Estate", "Tarsus", "Ostrogoth", "Thrak'dan", "Huzen", "Blood Rain Arena", "Terthorn",
-                                          "Bluestone Temple", "Devil's Tower", "Moonrose", "Jadestone", "Redmore Falls", "Korrizoth", "Lorbourne", "Fort Nox", "Tiravell",
-                                          "Stromgar", "The Machine Factory"})
-        CitySelector_txtBlock.Text = cities((36 * Rnd()) + 1)
+        Dim cities As New List(Of String)({"0 Index", "The Sunken City", "Viluun", "O'lan", "Vex", "Erengard", "North Rigton", "Ashvar", "Bromwich", "Bracklewhyte", "Azurehill", "Wharfton", "Kingdom of Storms",
+                                          "Duel'Tharmen", "Reymar", "Baruc", "Thorum", "Heggral", "Bhur Darul", "Kel Badur", "Manathus", "Tymarus", "Thrandul",
+                                          "Loria, The Angel's Harbor", "Requiem, the Unspoken City", "Collons", "Hierosolyma", "Dralun", "Brendenwal", "Istrus", "Callistrus", "Hartten", "Praetorium", "Rolan", "Derthona", "Augusto", "Aurinfort", "Tridenthia",
+                                          "Dul'tharion's Estate", "Tarsus", "Ostrogoth", "Legion of Giants Outpost", "Thrak'dan", "Huzen", "Blood Rain Arena", "Terthorn",
+                                          "Bluestone Temple", "Devil's Tower", "Moonrose", "Jadestone", "Redmore Falls", "Korrizoth", "Lorbourne", "Paythorne", "Alistar", "Shyrin", "Thurlstone", "Fort Nox", "Tiravell",
+                                          "Stromgar", "The Machine Factory"}) '61 cities
+        CitySelector_txtBlock.Text = cities((60 * Rnd()) + 1)
         NumberSelector_txtBlock.Text = Math.Round((49 * Rnd()) + 1).ToString + " location visited"
         Dim Spawn As Integer = Math.Round((99 * Rnd()) + 1).ToString
         If Spawn > 0 And Spawn < 6 Then
@@ -15,6 +15,7 @@
         Else
             SpawnSelector_txtBlock.Text = "No, Xur is not here!"
         End If
+
     End Sub
 
 
