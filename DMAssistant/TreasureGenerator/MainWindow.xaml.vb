a3f7b3fdca5e33a2095b98d51b6150a92a1569fe
Mark Roenfeldt <1187productions@gmail.com>
2021-08-18 23:38:42 -0600

    Started Treasure Generator

---
+++ DMAssistant/TreasureGenerator/MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Class MainWindow
+    Private Sub ComboBox_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+
+    End Sub
+
+    Private Sub ToggleButton_Click(sender As Object, e As RoutedEventArgs)
+        If sender.IsChecked Then
+
+        End If
+
+    End Sub
+End Class
