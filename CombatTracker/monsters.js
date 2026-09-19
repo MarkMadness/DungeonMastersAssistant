97f86ed7474b9b356a1d644567924fd7878e6bff
Mark Roenfeldt <1187productions@gmail.com>
2026-09-18 18:45:31 -0600

    added Allip

--- CombatTracker/monsters.js
+++ CombatTracker/monsters.js
@@ -29803,6 +29803,59 @@ const monstersLocal = [
         LairActions: [],
         Description: "Warlords are legendary battlefield commanders whose names are spoken with awe. After a string of decisive victories, a warlord could easily take on the role of monarch or general and attract followers willing to die for his or her banner."
     },
+    { // Allip
+        ID: 555,
+        ProfileType: "Monster",
+        Name: "Allip",
+        Type: "Medium undead, neutral evil",
+        TypeCategory: "Creature Type",
+        Source: "Mordenkainen's Tome of Foes",
+        HitPoints: 40,
+        HitPointsRoll: "9d8",
+        ArmorClass: [13, "natural armor"],
+        Speed: ["0 ft.", "fly 40 ft. (hover)"],
+        Strength: 6,
+        Dexterity: 17,
+        Constitution: 10,
+        Intelligence: 17,
+        Wisdom: 15,
+        Charisma: 16,
+        SavingThrows: ["Intelligence +6", "Wisdom +5"],
+        Skills: ["Perception +5", "Stealth +6"],
+        DamageVulnerabilities: [],
+        DamageResistances: ["Acid", "Fire", "Lightning", "Thunder", "Bludgeoning, Piercing, and Slashing from non magical weapons"],
+        DamageImmunities: ["Cold", "Necrotic", "Poison"],
+        ConditionImmunities: ["Charmed", "Exhaustion", "Frightened", "Grappled", "Paralyzed", "Petrified", "Poisoned", "Prone", "Restrained"],
+        Senses: ["Darkvision 60 ft.", "Passive Perception 15"],
+        Languages: ["the languages it knew in life"],
+        Challenge: [5, 1800],
+        ExtraRewards: "",
+        Traits: [
+            {
+                Title: "Incorporeal Movement",
+                Desc: "The allip can move through other creatures and objects as if they were difficult terrain. It takes 5 (1d10) force damage if it ends its turn inside an object."
+            }
+        ],
+        Actions: [
+            {
+                Title: "Maddening Touch",
+                Desc: "Melee Spell Attack: +6 to hit, reach 5 ft., one target. Hit: 17 (4d6 + 3) psychic damage."
+            },
+            {
+                Title: "Whispers of Madness",
+                Desc: "The allip chooses up to three creatures it can see within 60 feet of it. Each target must succeed on a DC 14 Wisdom saving throw, or it takes 7 (ld8 + 3) psychic damage and must use its reaction to make a melee weapon attack against one creature of the allip's choice that the allip can see. Constructs and undead are immune to this effect."
+            },
+            {
+                Title: "Howling Babble (Recharge 6)",
+                Desc: "Each creature within 30 feet of the allip that can hear it must make a DC 14 Wisdom saving throw. On a failed save, a target takes 12 (2d8 + 3) psychic damage, and it is stunned until the end of its next turn. On a successful save, it takes half as much damage and isn't stunned. Constructs and undead are immune to this effect."
+            }
+        ],
+        Reactions: [],
+        LegendaryActions: [],
+        LairActions: [],
+        RegionalEffects: [],
+        Description: "Description here"
+    },
 ];
 
 const uniqueLocal = [
